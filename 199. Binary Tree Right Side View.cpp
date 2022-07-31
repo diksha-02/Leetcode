23983901a19bf417a1f75e8437a0234ffa794773
Diksha Shukla <dikshashukla0201@gmail.com>
2022-07-31 21:29:04 +0530

    Create 199. Binary Tree Right Side View.cpp

---
+++ 199. Binary Tree Right Side View.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int>res;
+    void dfs(TreeNode* root,int height){
+        if(!root) return;
+        if(height==res.size()){
+            res.push_back(root->val);
+        }
+        dfs(root->right,height+1);
+        dfs(root->left,height+1);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        dfs(root,0);
+        return res;
+    }
+};
